131f9bb6cf3f99f9e90636712c87da3c1492eafc
sailabswarnim <sailab.endeavour@gmail.com>
2020-08-22 19:13:42 +0530

    Tables created with the description column

--- db/schema.rb
+++ db/schema.rb
@@ -14,6 +14,7 @@ ActiveRecord::Schema.define(version: 2020_08_22_131502) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
+    t.text "description"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
