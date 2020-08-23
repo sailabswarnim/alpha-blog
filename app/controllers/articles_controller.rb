77d452cb7ac6715288ed1dcddf12932fa37da5eb
sailabswarnim <sailab.endeavour@gmail.com>
2020-08-23 13:10:47 +0530

    Article creation from browser with flash messages

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,4 +8,20 @@ class ArticlesController < ApplicationController
     def index
       @articles = Article.all 
     end 
+
+    def new
+      @article = Article.new
+    end
+
+    def create
+      @article = Article.new(params.require(:article).permit(:title, :description))
+      #render plain: @article.inspect
+      if @article.save 
+        flash[:notice] = "Article was created successfully."
+
+      redirect_to @article
+      else 
+        render 'new'
+      end
+    end
 end
